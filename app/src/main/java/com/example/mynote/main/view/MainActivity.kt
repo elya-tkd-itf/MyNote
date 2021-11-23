5a2baacccc74ce34234960a7055cd5490603260c
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-11-23 14:36:05 +0300

    second homework

--- app/src/main/java/com/example/mynote/main/view/MainActivity.kt
+++ app/src/main/java/com/example/mynote/main/view/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.mynote.main.view
 
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.mynote.R
+import com.example.mynote.about.AboutActivity
 import com.example.mynote.main.model.MainModel
 import com.example.mynote.main.presenter.MainPresenter
 import com.example.mynote.main.presenter.NotePresenter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity(), NoteView {
 
@@ -17,6 +30,8 @@ class MainActivity : AppCompatActivity(), NoteView {
 
     private lateinit var nameEditText: EditText
     private lateinit var descriptionEditText: EditText
+    private lateinit var photoButton: FloatingActionButton
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +44,11 @@ class MainActivity : AppCompatActivity(), NoteView {
     private fun initViews() {
         nameEditText = findViewById(R.id.nameText)
         descriptionEditText = findViewById(R.id.descriptionText)
+        imageView = findViewById(R.id.imageView)
+        photoButton = findViewById(R.id.photoButton)
+        photoButton.setOnClickListener {
+            takePicture()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,11 +57,16 @@ class MainActivity : AppCompatActivity(), NoteView {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.save) {
-            presenter?.trySave(
+        when (item.itemId) {
+            R.id.save -> presenter?.trySave(
                 nameEditText.text.toString(),
                 descriptionEditText.text.toString()
             )
+            R.id.share -> shareNote(
+                nameEditText.text.toString(),
+                descriptionEditText.text.toString()
+            )
+            R.id.about -> openAboutScreen()
         }
         return true
     }
@@ -63,7 +88,95 @@ class MainActivity : AppCompatActivity(), NoteView {
         showToast(getString(R.string.emptySaveMessage))
     }
 
+    override fun shareNote(name: String, description: String) {
+        startActivity(Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, "$name\n$description")
+        })
+    }
+
+    override fun openAboutScreen() {
+        startActivity(Intent(this, AboutActivity::class.java))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> {
+                    Log.i(TAG, "User interaction was cancelled.")
+                }
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                    dispatchTakePictureIntent()
+                }
+                else -> {
+                    Log.i(TAG, "Permission denied.")
+                }
+            }
+        }
+    }
+
+    override fun takePicture() {
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        )
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+            this, Manifest.permission.CAMERA
+        )
+        if (shouldProvideRationale) {
+            showToast("Разрешите использование камеры в настройках приложения")
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+            imageView.layoutParams = LinearLayout.LayoutParams(imageView.width, 900)
+        }
+    }
+
     private fun showToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private val TAG = "MainActivity"
+        private val REQUEST_IMAGE_CAPTURE = 1
+        private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
     }
 }
