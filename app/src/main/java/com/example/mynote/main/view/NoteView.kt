0a7e16f70edda2ccb694723e5c730351bc5003c1
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-11-28 22:11:58 +0300

    fixes for 2 homework

--- app/src/main/java/com/example/mynote/main/view/NoteView.kt
+++ app/src/main/java/com/example/mynote/main/view/NoteView.kt
@@ -1,10 +1,13 @@
 package com.example.mynote.main.view
 
+import android.content.Intent
+
 interface NoteView {
     fun onSaveSuccessEvent()
     fun onSaveErrorEvent()
     fun onAttemptSaveEmptyContent()
-    fun shareNote(name: String, description: String)
-    fun openAboutScreen()
-    fun takePicture()
+    fun onAttemptShareEmptyContent()
+    fun showToast(text: String)
+    fun startMyActivity(intent: Intent)
+    fun startMyActivityForResult(intent: Intent, result: Int)
 }
